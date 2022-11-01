ede6a82611e060cd5e7c2c761a8b3858ce85dcdd
Chris Halabi <chridtianhalabi@gmail.com>
2022-11-01 17:24:55 +0200

    Fixing minor stuff

--- src/app/testimonials/testimonial.component.ts
+++ src/app/testimonials/testimonial.component.ts
@@ -7,24 +7,24 @@ import { Component} from '@angular/core';
 })
 export class TestimonialComponent{
   testimonials = [
-    {   
-      author: "John Doe",
-      job: "Web Developer",
-      img: "assets/testimonial-1.png",
-      info: "Chris was an awesome man.....",
+    {
+      author: "Hassan Khousheish",
+      job: "Software Engineer",
+      img: "assets/hassan.png",
+      info: "Chris was a real pleasure to work with and we look forward to working with him again. He's definitely the kind of designer you can trust with a project from start to finish.",
     },
-    { 
-      author: "hassan smtg",
-      job: "Software Developer",
+    {
+      author: "Daniel El Khoury",
+      job: "Web Developer",
       img: "assets/testimonial-2.png",
-      info: "Another text describing how great Chris was...",
+      info: "Christian's way of working was unique with a confidence to dig in to the harder parts of systems and has the collaborative friction that's needed to produce excellent work.",
     },
   ];
-  
+
   person = 0;
   btn1() {
     this.person = 0;
-    
+
   }
   btn2() {
     this.person = 1;
